9cc1afe576e69f65ea381987556f8a182070dea0
zakharov <zakharov@localhost>
2015-10-24 19:13:28 +0300

    commit 24.10.2015

--- application/views/peripherytemp_view.php
+++ application/views/peripherytemp_view.php
@@ -28,18 +28,7 @@
             font-family: arial;
         }
     </style>
-    
-     <style>
-        .scenario_window {height: 50px; background-color: #808080;}
-        .scenario_text{color: white;font-size: 1.1em;font-family: arial;}
-        .scenario_text2{color: white;font-size: 1.1em;font-family: arial; margin-top: 25px;}
-         body{background-color: dimgray;}
-        .img_style{height: 40px;}
-        .text_style {color: white;font-size: 19px;font-family: arial;}
-        .child {display: inline-block;vertical-align: middle;margin: 0 0.5em 0 0.5em;margin-top: 5px;}
-        .parent {word-wrap: break-word;text-align: left;}
-    </style>
-    
+     
     
     <div class="grid_8">
 	<div style="margin-top: 3%;">
@@ -83,31 +72,7 @@
 			</div>
                         <div class="grid_3">
                             
-                             <a href="/peripherylight" style="text-decoration: none;">
-                                    <div class="parent control_unit_container_scenarios ui-state-highlight main_div_unit scenario_window">
-                                            <div class="child">
-                                                    <img class="img_style" src="images/light.png">
-                                            </div>
-                                            <div class="child">
-                                                    <div class="text_style">
-                                                            Освещение
-                                                    </div>
-                                            </div>
-                                    </div>
-                            </a>
-                            
-                             <a href="/peripherytemp" style="text-decoration: none;">
-                                    <div class="parent control_unit_container_scenarios ui-state-highlight main_div_unit scenario_window">
-                                            <div class="child">
-                                                    <img class="img_style" src="images/temp.png">
-                                            </div>
-                                            <div class="child">
-                                                    <div class="text_style">
-                                                            Климат
-                                                    </div>
-                                            </div>
-                                    </div>
-                            </a>
+                            <?php include("partial/partial_periphery.php");?>
                             
 			</div>
 					
