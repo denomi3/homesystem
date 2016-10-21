86765533cded25d859a0935bc90af35c360c9f06
zakharov <zakharov@localhost>
2016-10-21 23:52:12 +0300

    commit 21.10.2016

--- application/models/model_peripheryblinds.php
+++ application/models/model_peripheryblinds.php
@@ -8,7 +8,7 @@ class Model_Peripheryblinds extends Model
                 global $dbconnection;            
                 
                 $common_sql = "SELECT `keyID` , `key_addr` , `key_title` , `key_label` , `key_pio` , `key_level` , `key_type` , `key_place`
-                               FROM `dev_list` where key_type='key'";
+                               FROM `dev_list` where key_type='motor'";
                 
                 if (!$common_result = $dbconnection->query($common_sql))
                 {
